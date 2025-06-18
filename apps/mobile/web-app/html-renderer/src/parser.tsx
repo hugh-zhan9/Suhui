5f687cbc9e92893e15a789e2ecc6c040a4bfe861
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-06-18 16:52:29 +0800

    chore: fix import in web app html-render

--- apps/mobile/web-app/html-renderer/src/parser.tsx
+++ apps/mobile/web-app/html-renderer/src/parser.tsx
@@ -1,6 +1,6 @@
 import { MemoedDangerousHTMLStyle } from "@follow/components/common/MemoedDangerousHTMLStyle.jsx"
 import { Checkbox } from "@follow/components/ui/checkbox/index.jsx"
-import { parseHtml as parseHtmlGeneral } from "@follow/components/ui/markdown/parse-html.js"
+import { parseHtml as parseHtmlGeneral } from "@follow/utils/html"
 import type { Components } from "hast-util-to-jsx-runtime"
 import { createElement } from "react"
 import { renderToString } from "react-dom/server"
@@ -65,7 +65,6 @@ export const parseHtml = (
         return createElement(MarkdownP, props, props.children)
       },
 
-      // @ts-expect-error
       math: Math,
       hr: ({ node, ...props }) =>
         createElement("hr", {
