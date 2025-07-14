1fe6324eb1f8d9353023910e563ab49d109287bd
DIYgod <i@diygod.me>
2025-07-14 16:45:14 +0800

    fix: subscription list loading status

--- apps/desktop/layer/renderer/src/modules/subscription-column/subscription-list/SubscriptionListGuard.tsx
+++ apps/desktop/layer/renderer/src/modules/subscription-column/subscription-list/SubscriptionListGuard.tsx
@@ -4,7 +4,7 @@ import { cn } from "@follow/utils"
 import { SubscriptionList as FeedListDesktop } from "./SubscriptionList"
 
 export const SubscriptionListGuard = function SubscriptionListGuard(props: SubscriptionProps) {
-  const { ref, className, view } = props
+  const { ref, className, view, isSubscriptionLoading } = props
 
   if (typeof view !== "number") {
     return null
@@ -14,12 +14,13 @@ export const SubscriptionListGuard = function SubscriptionListGuard(props: Subsc
       className={cn("flex size-full flex-col text-sm", className)}
       view={view}
       ref={ref}
+      isSubscriptionLoading={isSubscriptionLoading}
     />
   )
 }
 
 export type SubscriptionProps = ComponentType<
-  { className?: string; view: FeedViewType } & {
+  { className?: string; view: FeedViewType; isSubscriptionLoading: boolean } & {
     ref?: React.Ref<HTMLDivElement | null> | ((node: HTMLDivElement | null) => void)
   }
 >
