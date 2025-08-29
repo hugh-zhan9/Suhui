9bcfae41c27612409bee7e3a888348166d8b3dcf
Innei <tukon479@gmail.com>
2025-08-29 16:26:03 +0800

    chore(sentry): enhance Sentry configuration for improved error tracking
    - Updated Sentry configuration to include new integrations such as eventFilters, moduleMetadata, httpClient, and reactRouterV6BrowserTracing.
    - Expanded the list of ignored error patterns to cover more specific cases, improving error handling.
    - Removed redundant error handling logic from the Sentry initialization process, streamlining the setup.
    
    These changes aim to enhance error tracking capabilities and improve overall application stability.
    
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/desktop/layer/renderer/src/initialize/sentry.ts
+++ apps/desktop/layer/renderer/src/initialize/sentry.ts
@@ -1,11 +1,8 @@
 import { ELECTRON_BUILD } from "@follow/shared/constants"
-import { env } from "@follow/shared/env.desktop"
 import { whoami } from "@follow/store/user/getters"
 import { appSessionTraceId } from "@follow/utils/environment"
 import { version } from "@pkg"
 import { nanoid } from "nanoid"
-import { useEffect } from "react"
-import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from "react-router"
 
 import { SentryConfig } from "./sentry.config"
 
@@ -21,25 +18,7 @@ export const initSentry = async () => {
   if (!window.SENTRY_RELEASE) return
   if (import.meta.env.DEV) return
   const Sentry = await import("@sentry/react")
-  Sentry.init({
-    dsn: env.VITE_SENTRY_DSN,
-    environment: RELEASE_CHANNEL,
-    integrations: [
-      Sentry.moduleMetadataIntegration(),
-      Sentry.httpClientIntegration(),
-      Sentry.reactRouterV6BrowserTracingIntegration({
-        useEffect,
-        useLocation,
-        useNavigationType,
-        createRoutesFromChildren,
-        matchRoutes,
-      }),
-      Sentry.captureConsoleIntegration({
-        levels: ["error"],
-      }),
-    ],
-    ...SentryConfig,
-  })
+  Sentry.init(SentryConfig)
 
   const user = whoami()
   if (user) {
