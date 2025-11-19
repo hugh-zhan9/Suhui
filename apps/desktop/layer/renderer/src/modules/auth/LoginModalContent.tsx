b27b3a1fe0ccac4b073886a6e5daad0a473f0812
Innei <tukon479@gmail.com>
2025-11-19 16:06:03 +0800

    fix: conditionally render close button in LoginModalContent for non-Electron environments
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/desktop/layer/renderer/src/modules/auth/LoginModalContent.tsx
+++ apps/desktop/layer/renderer/src/modules/auth/LoginModalContent.tsx
@@ -4,6 +4,7 @@ import { Folo } from "@follow/components/icons/folo.js"
 import { Logo } from "@follow/components/icons/logo.js"
 import { MotionButtonBase } from "@follow/components/ui/button/index.js"
 import { useIsDark } from "@follow/hooks"
+import { IN_ELECTRON } from "@follow/shared"
 import type { LoginRuntime } from "@follow/shared/auth"
 import { stopPropagation } from "@follow/utils/dom"
 import { cn } from "@follow/utils/utils"
@@ -119,15 +120,15 @@ export const LoginModalContent = (props: LoginModalContentProps) => {
           <Folo className="size-12" />
         </m.div>
       </div>
-
-      <GlassButton
-        variant="flat"
-        className="absolute -right-2 -top-2 bg-material-medium"
-        onClick={modal.dismiss}
-      >
-        <i className="i-mgc-close-cute-re size-4" />
-      </GlassButton>
-
+      {!IN_ELECTRON && (
+        <GlassButton
+          variant="flat"
+          className="absolute -right-2 -top-2 bg-material-medium bg-transparent"
+          onClick={modal.dismiss}
+        >
+          <i className="i-mgc-close-cute-re size-4" />
+        </GlassButton>
+      )}
       {isEmail ? (
         <m.div
           initial={{ opacity: 0, y: 10 }}
